f3faaa7ae79e4725a812e1b9fa593716ff2e649c
Vinguyen <Vinguyen6996@gmail.com>
2021-03-21 21:56:17 +0700

    remove lib schedule from src

--- src/schedule_work/bryntum-resources/dummy.umd.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-function _objectDestructuringEmpty(obj) { if (obj == null) throw new TypeError("Cannot destructure undefined"); }
-
-_objectDestructuringEmpty(bryntum.schedulerpro); // Dummy import file used to import when feature detecting dynamic imports
-//export default '';
--- src/schedule_work/bryntum-schedulerpro/postcss.config.js
+++
@@ -1,4 +0,0 @@
-// This file enables loading css from node_module in vue-typescript demo
-module.exports = {
-
-};
