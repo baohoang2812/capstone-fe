ce352d20a90ba9840d8817a6a0d0d2143703fdfa
Vinguyen <Vinguyen6996@gmail.com>
2021-03-23 22:50:44 +0700

    Call Api Report

--- src/Core/Modules/Dashboard/Api/index.js
+++ src/Core/Modules/Dashboard/Api/index.js
@@ -1,12 +1,13 @@
 import BaseApi from "~/Core/Api/BaseAPI";
-class CamerasApi extends BaseApi {
-  getList = (filters) => {
-    return this.initApi.get(`${this.baseUrl}`, filters);
+class ReportApi extends BaseApi {
+  getList = (status) => {
+    return this.initApi.get(`${this.baseUrl}?Filter.IsDeleted=${false}&Filter.Status=${status}`);
   };
 
   getOne = (id) => {
     return this.initApi.get(`${this.baseUrl}?Filter.Ids=${id}`);
   };
+
   create = (body) => {
     return this.initApi.post(`${this.baseUrl}`, body);
   };
@@ -15,4 +16,4 @@ class CamerasApi extends BaseApi {
     return this.initApi.put(`${this.baseUrl}/${id}`, body);
   };
 }
-export default new CamerasApi().requestUrl("/v1/cameras");
+export default new ReportApi().requestUrl("/v1/reports");
--- src/Core/Modules/Dashboard/Components/CardTotalRevenue/index.js
+++ src/Core/Modules/Dashboard/Components/CardTotalRevenue/index.js
@@ -1,61 +1,61 @@
 import "./style.less";
 
 import React, { useState, useEffect } from "react";
-import { Card} from "antd";
+import { Card } from "antd";
 import { Bar } from '@ant-design/charts';
 /* Components */
 import HeaderCard from "./HeaderCard";
-
-/* Core helpers */
-
-/* Api */
+import reportApi from "~/Core/Modules/Dashboard/Api";
+import { values } from "lodash";
 
 
-const data = [
-  {
-    type: 'Chi nhánh 1',
-    sales: 38,
-  },
-  {
-    type: 'Chi nhánh 2',
-    sales: 52,
-  },
-  {
-    type: 'Chi nhánh 3',
-    sales: 61,
-  },
-  {
-    type:'Chi nhánh 4',
-    sales: 145,
-  },
-  {
-    type: 'Chi nhánh 5',
-    sales: 48,
-  },
-  {
-    type: 'Chi nhánh 6',
-    sales: 38,
-  },
-  {
-    type: 'Chi nhánh 7',
-    sales: 38,
-  },
-  {
-    type: 'Chi nhánh 8',
-    sales: 38,
-  },
-];
+// const data = [
+//   {
+//     type: 'Chi nhánh 1',
+//     sales: 38,
+//   },
+//   {
+//     type: 'Chi nhánh 2',
+//     sales: 52,
+//   },
+//   {
+//     type: 'Chi nhánh 3',
+//     sales: 61,
+//   },
+//   {
+//     type:'Chi nhánh 4',
+//     sales: 145,
+//   },
+//   {
+//     type: 'Chi nhánh 5',
+//     sales: 48,
+//   },
+//   {
+//     type: 'Chi nhánh 6',
+//     sales: 38,
+//   },
+//   {
+//     type: 'Chi nhánh 7',
+//     sales: 38,
+//   },
+//   {
+//     type: 'Chi nhánh 8',
+//     sales: 38,
+//   },
+// ];
 
 const CardTotalRevenue = () => {
   /* State */
-  const [loading, setLoading] = useState(true);
+
+  const [listReport, setListReport] = useState([]);
   const [filters, setFilters] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [config, setConfig] = useState({
-    data: data,
-    xField: 'sales',
-    yField: 'type',
-    seriesField: 'type',
-    color: function color(_ref) {
+    data: [],
+    xField: 'Point',
+    yField: 'Branch',
+    seriesField: 'Branch',
+    color:function color(_ref) {
       var type = _ref.type;
       return type === '美容洗护' ? '#FAAD14' : '#5B8FF9';
     },
@@ -65,7 +65,29 @@ const CardTotalRevenue = () => {
       sales: { alias: '销售额' },
     }
   });
+ 
 
+  useEffect(() => {
+    (async () => {
+
+      const list = await reportApi.getList("Done");
+      console.log(list,"List");
+      const data = list?.data?.result?.map((item) => (
+        {
+          Branch: item?.branch?.name,
+          Point: item?.totalMinusPoint
+        }
+      )
+      )
+      
+     setConfig({
+       ...config,
+       data:data,})
+       console.log(data);
+
+    })();
+  }, []);
+  
   const onInitData = () => {
     if (filters) {
       setLoading(true);
@@ -97,7 +119,7 @@ const CardTotalRevenue = () => {
     <div className="card-total-revenue card-default">
       <Card title={<HeaderCard setFilters={setFilters} />} bordered={false}>
         {/* <Spin spinning={loading}> */}
-          <Bar {...config} />
+        <Bar {...config} />
         {/* </Spin> */}
       </Card>
     </div>
