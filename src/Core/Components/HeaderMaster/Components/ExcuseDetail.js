c340dce10a3bd1b32365898e0ac3504458591ea5
Vinguyen <Vinguyen6996@gmail.com>
2021-03-30 11:31:21 +0700

    Update Notifi Header

---
+++ src/Core/Components/HeaderMaster/Components/ExcuseDetail.js
@@ -0,0 +1,197 @@
+import React, { useEffect, useState } from "react";
+import "./style.less";
+import { useDispatch } from "react-redux";
+import {
+    Row,
+    Col,
+    Form,
+    Modal,
+    Upload
+} from "antd";
+import moment from "moment";
+/* Hooks */
+import useTranslate from "~/Core/Components/common/Hooks/useTranslate";
+
+/* Actions */
+
+/* Constants */
+
+/* Api */
+import vioApi from "~/Core/Api/VioAPI";
+import employeeApi from "~/Core/Modules/Employee/Api";
+
+function getBase64(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    });
+}
+const ExcuseDetail = ({ form, action, ID }) => {
+    const t = useTranslate();
+    /* Redux */
+    /* State */
+    const { getFieldDecorator, validateFields } = form;
+    const [dataEmployee, setDataEmployee] = useState([]);
+    const [fileList, setFileList] = useState([]);
+    const [previewVisible, setPreviewVisible] = useState(false);
+    const [previewTitle, setPreviewTitle] = useState("");
+    const [previewImage, setPreviewImage] = useState(false);
+    const[data,setData]= useState([]);
+
+    useEffect(() => {
+        (async ()=> { 
+            const Vio = await vioApi.getOne(ID);
+            setData(Vio);
+        }
+        )()
+    },[ID]);
+    console.log(data,"DATA")
+    useEffect(() => {
+
+        (async () => {
+            if (data?.data?.result?.[0]?.employeeIds?.length > 0) {
+                const res = await employeeApi.getListFilter(data.employeeIds)
+                const result = res.data.result;
+                setDataEmployee(result);
+            } else {
+                setDataEmployee([])
+            }
+            if (data?.data?.result?.[0]?.evidence?.length > 0) {
+                const list = data?.data?.result?.[0]?.evidence?.map((item, index) => ({
+                    uid: index,
+                    name: "Evidence",
+                    status: "done",
+                    url: item.imagePath,
+                }))
+                setFileList(list);
+            }
+        })()
+
+    }, [data]);
+    const handleCancel = () => setPreviewVisible(false);
+
+    const handlePreview = async (file) => {
+        if (!file.url && !file.preview) {
+            file.preview = await getBase64(file.originFileObj);
+        }
+
+        setPreviewImage(file.url || file.preview)
+        setPreviewVisible(true)
+        setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1))
+    }
+   
+    return (
+        <Row type="flex" justify="center">
+            <Col span={24}>
+                <Form>
+                    <Row type="flex" justify="center" align="bottom">
+                        <Col span={8}>
+                            <Form.Item label={t("CORE.VIOLATION.NAME")}>
+                                {getFieldDecorator("name", {
+
+                                })(<span>{data?.data?.result?.[0]?.name}</span>)}
+                            </Form.Item>
+                        </Col>
+                        <Col span={8}>
+                            <Form.Item label={t("CORE.VIOLATION.CREATED.BY")}>
+                                {getFieldDecorator("createdBy", {
+
+                                })(<span>{data?.data?.result?.[0]?.createdBy?.lastName} {data?.data?.result?.[0]?.createdBy?.firstName}</span>)}
+                            </Form.Item>
+                        </Col>
+                        <Col span={4}>
+                            <Form.Item label={t("CORE.VIOLATION.CHARGE.CREATE")}>
+                                {getFieldDecorator("createdAt", {
+
+                                })(<span>{moment(data?.data?.result?.[0]?.createdAt).format("DD-MM-YYYY")}</span>)}
+                            </Form.Item>
+                        </Col>
+                    </Row>
+
+                    <Row type="flex" justify="center" align="bottom">
+                            <Col span={16}>
+                                <Form.Item label={t("CORE.VIOLATION.DESCRIPTION")}>
+                                    {getFieldDecorator("description", {
+
+                                    })(<span>{data?.data?.result?.[0]?.description}</span>)}
+                                </Form.Item>
+                            </Col>
+                            <Col span={4}>
+                                <Form.Item label={t("CORE.VIOLATION.WORKSPACE")}>
+                                    {getFieldDecorator("workspace", {
+
+                                    })(<span>{data?.data?.result?.[0]?.workspace?.[0]?.name}</span>)}
+                                </Form.Item>
+                            </Col>
+                        </Row>
+
+                    <Row type="flex" justify="center" align="bottom">
+                        <Col span={20}>
+                            <Form.Item label={t("CORE.VIOLATION.IMAGE.PATH")}>
+                                <Upload
+                                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                                    listType="picture-card"
+                                    fileList={fileList}
+                                    onPreview={handlePreview}
+                                    showUploadList={{
+                                        showRemoveIcon: false
+                                    }}
+                                >
+                                </Upload>
+                                <Modal
+                                    visible={previewVisible}
+                                    title={t("CORE.VIOLATION.IMAGE.PATH")}
+                                    footer={null}
+                                    onCancel={handleCancel}
+                                >
+                                    <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                                </Modal>
+
+                            </Form.Item>
+                        </Col>
+                    </Row>
+
+                    <Row type="flex" justify="center" align="bottom">
+                        <Col span={20}>
+                            <Form.Item label={t("CORE.VIOLATION.VIOLATOR")}>
+                                {getFieldDecorator('select-multiple', {
+
+                                })(
+                                    <>
+                                        {
+                                            dataEmployee.map(item => {
+                                                return (
+                                                    <div>
+                                                        {`${item?.lastName} ${item?.firstName}`}
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </>
+                                )}
+                            </Form.Item>
+                        </Col>
+                    </Row>
+
+                    <Row type="flex" justify="center" align="bottom">
+                        {
+                            data?.status === "Excused" || data?.status === "Rejected" ? (<Col span={20}>
+                                <Form.Item label={t("CORE.VIOLATION.EXCUSE")}>
+                                    {getFieldDecorator("excuse", {
+
+                                    })(<span>{data?.data?.result?.[0].excuse}</span>)}
+                                </Form.Item>
+                            </Col>) : null
+                        }
+                    </Row>
+                   
+                </Form>
+            </Col>
+        </Row>
+    );
+};
+export default Form.create({ name: "Excuse_Detail" })(
+    ExcuseDetail
+);
--- src/Core/Components/HeaderMaster/MenuNotify.js
+++ src/Core/Components/HeaderMaster/MenuNotify.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Avatar, List } from "antd";
-import { Link} from "react-router-dom";
+import {
+  Avatar,
+  List,
+  Modal
+} from "antd";
+import { Link } from "react-router-dom";
 import notiApi from "~/Core/Modules/Notification/Api";
 import moment from "moment";
+import useTranslate from "~/Core/Components/common/Hooks/useTranslate";
+
 /* Actions */
 // import { updateListNotification } from "~/Core/Store/actions/notification";
 
@@ -11,7 +17,8 @@ import moment from "moment";
 // import AttentionImg from "~/Core/Modules/Notification/assets/Attention.png";
 
 export default ({
-  handleOpenModal
+  setData,
+  openModelExcuse
 }) => {
   // const listNotification = useSelector(state => state.NotificationCore.listNotificationHeader)
 
@@ -19,55 +26,88 @@ export default ({
     // dispatch(updateListNotification("all"));
   }
   const [listNoti, setListNoti] = useState([]);
-
+  const t = useTranslate();
   useEffect(() => {
-    ( async () => {
+    (async () => {
       const res = await notiApi.getList(0, 5);
-        if (res.code !== 200) {
+      if (res.code !== 200) {
 
-          return;
-        }
-        const data = res?.data?.result || [];
-        setListNoti(data);
+        return;
+      }
+      const data = res?.data?.result || [];
+      setListNoti(data);
     })()
-   
+
   }, [])
 
-  const onClick = (e, item) => {
-    e.preventDefault();
-    handleOpenModal(item);
+  const onClick = (item) => {
+   changeStatus(item?.notification?.id)
     return;
+  };
+
+  const changeStatus= (id)=>{
+    const res = notiApi.update(id,{ isRead:true});
+    if (res.code !== 200) {
+      return;
+    }
+  }
+  const renderLink = (item) => {
+
+    if (item?.notification?.type === "Report") {
+      return (
+        <Link to={`/report/${item?.notification?.navigationId}`}>{item?.notification?.name}</Link>
+      )
+    }
+    else if (item?.notification?.type === "Violation") {
+      return (
+        <Link onClick={() => openModelExcuse(item?.notification?.navigationId)}>{item?.notification?.name}</Link>
+      )
+    }
+    else if (item?.notification?.type === "Workschedule") {
+      return (
+        <Link to={`/workSchedule/`}>{item?.notification?.name}</Link>
+      )
+    }
+    else if (item?.notification?.type === "Info") {
+      return (
+        <Link>{item?.notification?.name}</Link>
+      )
+    }
+    
   }
 
+
   return (
-    <div className = "dropdown-menu-notify">
-      <h4 className = "title" style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+    <>
+    <div className="dropdown-menu-notify">
+      <h4 className="title" style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
         <span>
           Thông báo mới nhận
         </span>
-        <div style ={{ padding: "0 10px" }} onClick={onMarkAllAsRead}>
-          <svg width ="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d ="M8 11L11 14L22 3" stroke="#2B3D4B" strokeLinecap="round" strokeLinejoin="round" />
-            <path d ="M20 12V19C20 20.1046 19.1046 21 18 21H4C2.89543 21 2 20.1046 2 19V5C2 3.89543 2.89543 3 4 3H15" stroke="#2B3D4B" strokeLinecap="round" strokeLinejoin="round" />
+        <div style={{ padding: "0 10px" }} onClick={onMarkAllAsRead}>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 11L11 14L22 3" stroke="#2B3D4B" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M20 12V19C20 20.1046 19.1046 21 18 21H4C2.89543 21 2 20.1046 2 19V5C2 3.89543 2.89543 3 4 3H15" stroke="#2B3D4B" strokeLinecap="round" strokeLinejoin="round" />
           </svg>
         </div>
       </h4>
 
       <List
-        itemLayout ="horizontal"
-        dataSource ={listNoti}
-        renderItem ={item => (
+        itemLayout="horizontal"
+        dataSource={listNoti}
+        renderItem={item => (
           <Link
-            onClick ={(e) => onClick(e, item)}
-            to ="#"
+            onClick={() => onClick(item)}
           >
-            <List.Item className ={!item.isRead && "unread"}>
+            <List.Item className={!item.isRead && "unread"}>
               <List.Item.Meta
-                avatar ={<Avatar shape="square" size="large" />}
-                title ={<span>{item?.notification?.name}</span>}
-                description ={
-                  <div className ="description">
-                    <p style ={{
+                avatar={<Avatar shape="square" size="large" />}
+                title={
+                  renderLink(item)
+                }
+                description={
+                  <div className="description">
+                    <p style={{
                       display: "-webkit-box",
                       WebkitLineClamp: 2,
                       WebkitBoxOrient: "vertical",
@@ -79,11 +119,15 @@ export default ({
                   </div>
                 }
               />
+             
             </List.Item>
+           
           </Link>
         )}
       />
-      <Link to ="/notification/" className="link-read-all">Xem tất cả</Link>
+      
+      <Link to="/notification/" className="link-read-all">Xem tất cả</Link>     
     </div>
+   </>
   )
 }
