776df89ab3ccee5e07e7c297c264a2292a113474
Vinguyen <Vinguyen6996@gmail.com>
2021-04-01 22:04:26 +0700

    Format day

--- src/Core/Modules/Report/Components/Form/ViolationDetail.js
+++ src/Core/Modules/Report/Components/Form/ViolationDetail.js
@@ -125,21 +125,21 @@ const ViolationDetail = ({ form, is_create, action, data }) => {
                             <Form.Item label={t("CORE.VIOLATION.NAME")}>
                                 {getFieldDecorator("name", {
 
-                                })(<span>{data?.name}</span>)}
+                                })(<span style={{fontWeight:800}}>{data?.name}</span>)}
                             </Form.Item>
                         </Col>
                         <Col span={8}>
                             <Form.Item label={t("CORE.VIOLATION.CREATED.BY")}>
                                 {getFieldDecorator("createdBy", {
 
-                                })(<span>{data?.createdBy?.lastName} {data?.createdBy?.firstName}</span>)}
+                                })(<span style={{fontWeight:800}}>{data?.createdBy?.lastName} {data?.createdBy?.firstName}</span>)}
                             </Form.Item>
                         </Col>
                         <Col span={4}>
                             <Form.Item label={t("CORE.VIOLATION.CHARGE.CREATE")}>
                                 {getFieldDecorator("createdAt", {
 
-                                })(<span>{moment(data?.createdAt).format("DD-MM-YYYY")}</span>)}
+                                })(<span style={{fontWeight:800}}>{moment(data?.createdAt).format("DD-MM-YYYY | HH:mm")}</span>)}
                             </Form.Item>
                         </Col>
                         </Row>
@@ -149,14 +149,14 @@ const ViolationDetail = ({ form, is_create, action, data }) => {
                                 <Form.Item label={t("CORE.VIOLATION.DESCRIPTION")}>
                                     {getFieldDecorator("description", {
 
-                                    })(<span>{data?.description}</span>)}
+                                    })(<span style={{fontWeight:800}}>{data?.description}</span>)}
                                 </Form.Item>
                             </Col>
                             <Col span={4}>
                                 <Form.Item label={t("CORE.VIOLATION.WORKSPACE")}>
                                     {getFieldDecorator("workspace", {
 
-                                    })(<span>{data?.workspace?.[0]?.name}</span>)}
+                                    })(<span style={{fontWeight:800}}>{data?.workspace?.[0]?.name}</span>)}
                                 </Form.Item>
                             </Col>
                         </Row>
@@ -194,10 +194,10 @@ const ViolationDetail = ({ form, is_create, action, data }) => {
                                     })(
                                         <>
                                             {
-                                                dataEmployee.map(item => {
+                                                dataEmployee?.map(item => {
                                                     return (
                                                         <div>
-                                                            {`${item.lastName} ${item.firstName}`}
+                                                            {`${item?.lastName} ${item?.firstName}`}
                                                         </div>
                                                     )
                                                 })
