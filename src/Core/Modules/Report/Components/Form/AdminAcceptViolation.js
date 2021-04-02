ae0226ed8d91645e153efaa9b939aceb8db49f15
Vinguyen <Vinguyen6996@gmail.com>
2021-04-02 17:05:56 +0700

    Fix invalid updatedAt

--- src/Core/Modules/Report/Components/Form/AdminAcceptViolation.js
+++ src/Core/Modules/Report/Components/Form/AdminAcceptViolation.js
@@ -66,6 +66,9 @@ const UpdateViolatorDetail = ({ form, isShow = true, action, data }) => {
             }))
             setFileList(list);
         }
+        else{
+            setFileList([]);
+        }
 
     }, [data]);
     const handleCancel = () => setPreviewVisible(false);
