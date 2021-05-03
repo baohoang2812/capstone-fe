b3a7c306bcba380f3719c13c99b5a648204abfcd
Nguyễn Quang Khải <nguyenquangkhai2210@gmail.com>
2021-05-03 09:55:57 +0700

    UPDATE

--- src/Core/Modules/Simulator/subRouter.js
+++ src/Core/Modules/Simulator/subRouter.js
@@ -9,7 +9,7 @@ import intlMessagesEN from "./i18n/localization/en.json";
 import intlMessagesVi from "./i18n/localization/vi.json";
 
 export default {
-  roleName: ["Staff", "System Admin", "Branch Manager"],
+  roleName: ["Staff", "System Admin"],
   name: "Simulator",
   dir: "Simulator",
   pathRoot: "simulator",
